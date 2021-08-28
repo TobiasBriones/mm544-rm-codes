16a01d341d8fef9624089de59d1c74f410627f2e
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-08-28 17:38:00 -0600

    Update copyright license headers

--- src/components/Examples.vue
+++ src/components/Examples.vue
@@ -1,3 +1,18 @@
+<!--
+  - Copyright (c) 2021 Tobias Briones. All rights reserved.
+  -
+  - SPDX-License-Identifier: BSD-3-Clause
+  -
+  - This file is part of Course Project at UNAH-MM544: Reed Muller Codes.
+  -
+  - The underlying theoretical contents bibliography is found in the
+  - BIBLIOGRAPHY.md file in the root directory of this project.
+  -
+  - This source code is licensed under the BSD-3-Clause License found in the
+  - LICENSE file in the root directory of this source tree or at
+  - https://opensource.org/licenses/BSD-3-Clause.
+  -->
+
 <template>
   <div>
     <p>
