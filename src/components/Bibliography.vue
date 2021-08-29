6a7b6764dce0174ddec111b4733bc2fe97eaf10e
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-08-28 22:57:31 -0600

    Add "a" tags for source links

--- src/components/Bibliography.vue
+++ src/components/Bibliography.vue
@@ -23,46 +23,68 @@
 
     <ul>
       <li>
-        Cherowitzo, B. (n.d.). Reed-Muller Codes. Www-Math.Ucdenver.Edu. Retrieved April 6, 2021,
-        from http://www-math.ucdenver.edu/~wcherowi/courses/m7823/reedmuller.pdf
-      </li>
-
-      <li>
         Abbe, E., Shpilka, A., & Ye, M. (2020, June 10). Reed-Muller Codes: Theory and Algorithms.
-        Arxiv.Org. https://arxiv.org/pdf/2002.03317.pdf
+        Arxiv.Org.
+        <a href="https://arxiv.org/pdf/2002.03317.pdf">
+          https://arxiv.org/pdf/2002.03317.pdf
+        </a>
       </li>
 
       <li>
         AMS American Mathematical Society. (n.d.-a). Digital Revolution (Part III) - Error Correction
         Codes. AMS. Retrieved April 6, 2021,
-        from http://www.ams.org/publicoutreach/feature-column/fcarc-errors6
+        from
+        <a href="https://www.ams.org/publicoutreach/feature-column/fcarc-errors6">
+          https://www.ams.org/publicoutreach/feature-column/fcarc-errors6
+        </a>
       </li>
 
       <li>
         Juan Benvenuto, C. (2012, May 31). Galois Field in Cryptography.
-        Sites.Math.Washington.Edu. https://sites.math.washington.edu/~morrow/336_12/papers/juan.pdf
+        Sites.Math.Washington.Edu.
+        <a href="https://sites.math.washington.edu/~morrow/336_12/papers/juan.pdf">
+          https://sites.math.washington.edu/~morrow/336_12/papers/juan.pdf
+        </a>
       </li>
 
       <li>
         Arriba De La Hera, A. (2016, June 20). Códigos de Reed-Muller.
         Addi.Ehu.Es.
-        https://addi.ehu.es/bitstream/handle/10810/20121/TFG_DeArribaDeLaHeraAndoni.pdf;jsessionid=B1A0849B5D5BE3CAF3F3DBB4E2013A49?sequence=2
+        <a href="https://addi.ehu.es/bitstream/handle/10810/20121/TFG_DeArribaDeLaHeraAndoni.pdf;jsessionid=B1A0849B5D5BE3CAF3F3DBB4E2013A49?sequence=2">
+          https://addi.ehu.es/bitstream/handle/10810/20121/TFG_DeArribaDeLaHeraAndoni.pdf;jsessionid=B1A0849B5D5BE3CAF3F3DBB4E2013A49?sequence=2
+        </a>
       </li>
 
       <li>
         Xambó-Descamps, S. (n.d.). ICT12. Web.Mat.Upc.Edu. Retrieved June 4, 2021,
-        from https://web.mat.upc.edu/sebastia.xambo/ICT/ICT12-06-LinearCodes-ControlMatrix.pdf
+        from
+        <a href="https://web.mat.upc.edu/sebastia.xambo/ICT/ICT12-06-LinearCodes-ControlMatrix.pdf">
+          https://web.mat.upc.edu/sebastia.xambo/ICT/ICT12-06-LinearCodes-ControlMatrix.pdf
+        </a>
       </li>
 
       <li>
         Pc, A., Rajendra Gad, V., & S Gad, R. (2019, November). Polynomials in Error Detection and
         Correction in Data Communication System.
         Www.Researchgate.Net.
-        https://www.researchgate.net/publication/337689159_Polynomials_in_Error_Detection_and_Correction_in_Data_Communication_System
+        <a href="https://www.researchgate.net/publication/337689159_Polynomials_in_Error_Detection_and_Correction_in_Data_Communication_System">
+          https://www.researchgate.net/publication/337689159_Polynomials_in_Error_Detection_and_Correction_in_Data_Communication_System
+        </a>
       </li>
 
       <li>
-        J. (2017). GitHub - jihoonseo/reed-muller. GitHub. https://github.com/jihoonseo/reed-muller
+        Cherowitzo, B. (n.d.). Reed-Muller Codes. Www-Math.Ucdenver.Edu. Retrieved April 6, 2021,
+        from
+        <a href="http://www-math.ucdenver.edu/~wcherowi/courses/m7823/reedmuller.pdf">
+          http://www-math.ucdenver.edu/~wcherowi/courses/m7823/reedmuller.pdf
+        </a>
+      </li>
+
+      <li>
+        J. (2017). GitHub - jihoonseo/reed-muller. GitHub.
+        <a href="https://github.com/jihoonseo/reed-muller">
+          https://github.com/jihoonseo/reed-muller
+        </a>
       </li>
     </ul>
   </div>
